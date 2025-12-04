76f6c82322060339b0f30a52c9b49ac7f34f45ee
2dam <rvm666@educa.madrid.org>
2025-12-04 12:32:31 +0100

    Modelo personaje dragon ball

--- MOVILES/AppDragonBallApi/app/src/main/java/com/example/appdragonballapi/domain/model/DragonBallCharacter.kt
+++ MOVILES/AppDragonBallApi/app/src/main/java/com/example/appdragonballapi/domain/model/DragonBallCharacter.kt
@@ -1,3 +1,22 @@
 package com.example.appdragonballapi.domain.model
 
-data class DragonBallCharacter()
+data class DragonBallCharacter(
+    val id: Int,
+    val name: String,
+    val ki: String,
+    val maxKi: String,
+    val race: String,
+    val gender: String,
+    val description: String,
+    val image: String,
+    val affiliation: String,
+    val deletedAt: Any
+)
+
+data class Transformation(
+    val deletedAt: Any,
+    val id: Int,
+    val image: String,
+    val ki: String,
+    val name: String
+)
