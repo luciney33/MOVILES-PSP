8bfb92a1e3e18aee7786a0b1315cd5a12356fa4e
luciapenafiel <lpenafiel@cofrai.com>
2026-02-21 14:57:40 +0100

    borrado el metodo sin uso en secretorepo

--- PSP/EmailSpring/src/main/java/org/example/emailspring/data/SecretoRepository.java
+++ PSP/EmailSpring/src/main/java/org/example/emailspring/data/SecretoRepository.java
@@ -4,10 +4,7 @@ import org.example.emailspring.data.entity.SecretoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface SecretoRepository extends JpaRepository<SecretoEntity, Long> {
-    List<SecretoEntity> findByAutorId(Long autorId);
 }
 
